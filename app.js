1d2f91f8154c2145fdf27f2c88265b7fdff9faf5
Animesh Kumar <animesh.kumar@successive.tech>
2022-12-20 19:24:35 +0530

    feat/users: create trainner module

--- app.js
+++ app.js
@@ -7,27 +7,32 @@ app.use(express.json());
 
 app.configure(express.rest());  
 
-app.use('/', new UserService());
+app.use('/user', new UserService());
+app.use('/trainer', new UserService());
 
 app.use(express.urlencoded({ extended:true }))
 
 app.use(express.errorHandler());
 
-app.service('/').findAll();
+// app.service('/').findAll();
 
-app.service('/').findById();
+// app.service('/').findById();
 
-app.service('/').on('created', user => {
+app.service('/user').on('created', user => {
     console.log('A new user has been created', user);
 });
 
-app.service('/').on('updated', user => {
-    console.log('A user has been updated', user);
-})
-
-app.service('/').on('removed', (removedUser)=>{
-    console.log('A user has been deleted', removedUser);
+app.service('/trainer').on('created', trainer => {
+    console.log('A new trainer has been created', trainer);
 });
+
+// app.service('/user').on('updated', user => {
+//     console.log('A user has been updated', user);
+// })
+
+// app.service('/user').on('removed', (removedUser)=>{
+//     console.log('A user has been deleted', removedUser);
+// });
 
 app.listen(process.env.PORT ).on('listening', ()=>{
     console.log(`A feather application is started on localhost: ${process.env.PORT}`); 
--- src/module/user.js
+++ src/module/user.js
@@ -13,31 +13,30 @@ const {
 
 const driver = neo4j.driver(url, neo4j.auth.basic(db_username, db_password));
 const session = driver.session({ database });
-
 class UserService {
-    async findAll() {
-        const result = await session.run(`Match (u:User) return u`)
-        return result.records.map(i=>i.get('u').properties)
-    }
-    async findById(id) {
-        const result = await session.run(`MATCH (u:User {_id : '${id}'} ) return u limit 1`)
-        return result.records[0].get('u').properties
-    }
+    // async findAll() {
+    //     const result = await session.run(`Match (u:User) return u`)
+    //     return result.records.map(i=>i.get('u').properties)
+    // }
+    // async findById(id) {
+    //     const result = await session.run(`MATCH (u:User {_id : '${id}'} ) return u limit 1`)
+    //     return result.records[0].get('u').properties
+    // }
     
     async create(user) {
         const unique_id = nanoid(8)
-        await session.run(`CREATE (u:User {_id : '${unique_id}', name: '${user.name}', mobile: '${user.mobile}', email: '${user.email}', password: '${user.password}'} ) return u`)
-        return await findById(unique_id)
+        const newUser = await session.run(`CREATE (u:User {_id : '${unique_id}', name: '${user.name}', mobile: '${user.mobile}', email: '${user.email}', password: '${user.password}'} ) return u`)
+        return await newUser;
     }
     
     async findByIdAndUpdate(id, user) {
         const result = await session.run(`MATCH (u:User {_id : '${id}'}) SET u.name= '${user.name}', u.mobile= '${user.mobile}', u.email= '${user.email}', u.password= '${user.password}' return u`)
-        return result.records[0].get('u').properties
+        return result
     }
     
     async findByIdAndDelete(id) {
-        await session.run(`MATCH (u:User {_id : '${id}'}) DELETE u`)
-        return await findAll()
+        const deleteUser = await session.run(`MATCH (u:User {_id : '${id}'}) WHERE u.deleted_by IS NULL return u`)
+        return await deleteUser;
     }
 }
 
